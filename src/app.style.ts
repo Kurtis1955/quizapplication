870ec613a0ecae1fcf910a662c9b515ea8989d2a
Theophilus Akowuah <118230741+Kurtis1955@users.noreply.github.com>
2024-07-18 02:10:32 +0100

    Create app.style.ts

---
+++ src/app.style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const AppStyle = styled.main`
+    width: 90%;
+    margin: 0 auto;
+    padding-top: 30px;
+    @media screen and (min-width: 768px) {
+        width: 80%;
+    }
+
+    @media screen and (min-width: 1024px) {
+        width: 95%;
+    }
+`
