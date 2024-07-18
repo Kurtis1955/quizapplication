8957219dca25a898142b7300535d59de7b50a29c
Theophilus Akowuah <118230741+Kurtis1955@users.noreply.github.com>
2024-07-18 02:31:32 +0100

    Create index.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,22 @@
+import HtmlIcon from "../assets/images/htmlicon.svg"
+import CssIcon from "../assets/images/cssicon.svg"
+import JSIcon from "../assets/images/jsicon.svg"
+import AccessibilityIcon from "../assets/images/accessibilityicon.svg"
+
+
+
+export const subjectToIcon = {
+    "HTML": HtmlIcon,
+    "CSS": CssIcon,
+    "JavaScript": JSIcon,
+    "Accessibility": AccessibilityIcon
+}
+
+export const QuizTitle = {
+    "HTML": 0,
+    "CSS": 1,
+    "JavaScript": 2,
+    "Accessibility": 3
+}
+  
+export const Letters = ["A", "B", "C", "D"]
