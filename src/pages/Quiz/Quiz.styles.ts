84e64cbeef10fc4215c1bf0b1f4c0f11cd7be55f
Theophilus Akowuah <118230741+Kurtis1955@users.noreply.github.com>
2024-07-19 04:08:19 +0100

    a

---
+++ src/pages/Quiz/Quiz.styles.ts
@@ -0,0 +1,221 @@
+import styled from "styled-components";
+
+type StyledOptionIndexProps = {
+    selected?: boolean;
+    isCorrectAnswer?: boolean | null;
+    isSubmitClicked?: boolean | null;
+}
+
+type ThemeProps = {
+    isSelected?: boolean;
+}
+
+export const StyledQuiz = styled.section`
+    margin-top: 55px;
+
+    @media screen and (min-width: 1024px) {
+        margin-top: 20px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        gap: 50px;
+    }
+
+    @media screen and (min-width: 1280px) {
+        margin-top: 60px;
+    }
+
+`
+
+export const StyledQuizContent = styled.div`
+
+    @media screen and (min-width: 1024px) {
+        width: 50%;
+        height: max-content;
+    }
+
+`
+
+export const StyledNumber = styled.div<ThemeProps>`
+    color: ${({ isSelected }) => (isSelected ? "#ABC1E1;" : "#626C7F;")}
+    font-size: 14px;
+    font-style: italic;
+    line-height: 150%;
+
+    @media screen and (min-width: 768px) {
+        font-size: 20px;
+    }
+    @media screen and (min-width: 1024px) {
+        font-size: 20px;
+    }
+`
+
+export const StyledQuestion = styled.div<ThemeProps>`
+    font-weight: 500;
+    font-size: 20px;
+    color: ${({ isSelected }) => (isSelected ? "#FFFFFF;" : "#313E51;")}
+    line-height: 120%;
+    margin-top: 10px;
+
+    @media screen and (min-width: 768px) {
+        font-size: 36px;
+    }
+`
+
+export const StyledProgress = styled.div`
+    margin: 20px 0;   
+    
+    @media screen and (min-width: 1024px) {
+        margin-top: 25%;
+    }
+`
+
+export const StyledOptions = styled.section`
+    display: flex;
+    flex-direction: column;
+    margin-top: 20px;
+    gap: 10px;
+
+    @media screen and (min-width: 768px) {
+        gap: 20px;
+    }
+
+    @media screen and (min-width: 1024px) {
+        width: 50%;
+        margin-top: 0;
+    }
+`
+
+export const StyledOption = styled.div<ThemeProps>`
+    color: ${({ isSelected }) => (isSelected ? "#FFFFFF;" : "#313E51;")}
+    font-size: 18px;
+    font-weight: 500;
+
+    @media screen and (min-width: 768px) {
+        font-size: 28px;
+    }
+`
+
+export const StyledOptionIndex = styled.div<StyledOptionIndexProps>`
+    background-color: ${({ selected, isCorrectAnswer, isSubmitClicked }) =>
+        ( selected && isSubmitClicked && isCorrectAnswer
+        ? "#26D782"
+        : selected && isSubmitClicked
+        ? "#EE5454"
+        : selected ? "#A729F5" : "#F4F6FA"
+        )};
+    color: ${({ selected, isCorrectAnswer, isSubmitClicked }) =>
+        ( selected && isSubmitClicked && isCorrectAnswer
+        ? "#ffffff"
+        : selected && isSubmitClicked
+        ? "#ffffff"
+        : selected ? "#ffffff" : "#626C7F"
+        )};
+    border-radius: 6px;
+    padding: 12px 16px;
+
+    &:hover {
+        
+    }
+
+    @media screen and (min-width: 768px) {
+        font-size: 28px;
+    }
+`
+// 
+
+export const StyledError = styled.div`
+    color: red;
+    text-align: center;
+    margin-top: 3px;
+`
+
+export const ResultCard = styled.section<ThemeProps>`
+    display: flex;
+    flex-direction: column;
+    justify-items: center;
+    align-items: center;
+    gap: 16px;
+    background-color: ${({ isSelected }) => (isSelected ? "#3B4D66;" : "#FFFFFF;")}
+    padding: 12px;
+    border-radius: 12px;
+    margin-top: 45px;
+    box-shadow: -1px 5px 13px 0px ${({ isSelected }) => ( isSelected ? "" : "#F4F6FA;")}
+    margin-bottom: 3px;
+
+    @media screen and (min-width: 768px) {
+        padding-top: 32px;
+        padding-bottom: 32px;
+    }
+
+    @media screen and (min-width: 1024px) {
+        margin-top: 0px;
+    }
+`
+
+export const CorrectAnswer = styled.section<ThemeProps>`
+    color: ${({ isSelected }) => (isSelected ? "#FFFFFF;" : "#313E51;")}
+    font-size: 88px;
+    font-weight: 500;
+`
+
+export const TotalQuestion = styled.section<ThemeProps>`
+
+    color: ${({ isSelected }) => (isSelected ? "#ABC1E1;" : "#626C7F;")}
+    font-size: 18px;
+
+    @media screen and (min-width: 768px) {
+        font-size: 24px;
+    }
+`
+
+export const Image = styled.img`
+    margin-left: auto;
+
+    @media screen and (min-width: 768px) {
+        width: 40px;
+        height: 40px;
+    }
+`
+
+export const ResultCardImage = styled(Image)`
+    margin-left: 0;
+    width: 25px;
+    height: 25px;
+
+    @media screen and (min-width: 768px) {
+        width: 56px;
+        height: 56px;
+    }
+`
+
+export const ErrorWrapper = styled.section`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 5px;
+    margin-top: 8px;
+
+    @media screen and (min-width: 768px) {
+        margin-top: 12px;
+    }
+`
+export const ErrorMessage = styled.p`
+    color: #EE5454;
+    font-size: 18px;
+    font-weight: 400;
+    margin-right: auto;
+
+    @media screen and (min-width: 768px) {
+        font-size: 24px;
+    }
+`
+
+export const Result = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    @media screen and (min-width: 1024px) {
+        width: 50%;
+    }
+`
---
+++ src/pages/Quiz/index.tsx
@@ -0,0 +1,271 @@
+import { useContext, useEffect, useState } from "react"
+import { AppContext } from "../../context/Appcontext"
+import { useLocation } from "react-router-dom"
+import {
+  CorrectAnswer, ErrorMessage, ErrorWrapper, Image,
+  Result, ResultCard,
+  StyledNumber, StyledOption, StyledOptionIndex,
+  StyledOptions, StyledProgress, StyledQuestion,
+  StyledQuiz, StyledQuizContent, TotalQuestion
+} from "./Quiz.styles"
+import Card from "../../components/Card"
+import Button from "../../components/Button"
+import { AcWrapper, CssWrapper, HtmlWrapper, JsWrappper, StyledLink, StyledQuizTitle, StyledSubtitle, StyledTitle, WelcomeTitle } from "../Home/Home.styles"
+import { ButtonStyle } from "../../components/Button/Button.styles"
+import { IconTitleWrapper } from "../../components/Navbar/Navbar.styles"
+import IconCorrect from "../../assets/images/icon-correct.svg"
+import IconError from "../../assets/images/icon-error.svg"
+import ProgressBar from "../../components/ProgressBar"
+import { subjectToIcon, QuizTitle, Letters } from "../../lib"
+
+
+export default function Quiz() {
+  const [activeQuestion, setActiveQuestion] = useState(0)
+  const [optionClicked, setOptionClicked] = useState<number | null>(null)
+  const [isCorrectAnswer, setIsCorrectAnswer] = useState<boolean | null>(null)
+  const [isSubmitClicked, setIsSubmitClicked] = useState(false)
+  const [showResult, setShowResult] = useState(false)
+  const [isActualAnswer, setIsActualAnswer] = useState("")
+  const [keyboardClick, setKeyBoardClick] = useState<boolean | null>(null)
+  const { isSelected } = useContext(AppContext)
+  // to check if a user clicked the submit without selecting an option
+  const [clicked, setClicked] = useState(false)
+  const [result, setResult] = useState({
+    correct: 0,
+    wrong: 0,
+  })
+
+  const { quiz } = useContext(AppContext)
+
+  const { pathname } = useLocation()
+  const quizSubject = pathname.substring(1).split("/").pop()
+  
+  const quizQuestions = quiz[QuizTitle[quizSubject as keyof typeof QuizTitle]]
+
+  const questions = quizQuestions.questions
+
+  const actualAnswer = questions[activeQuestion]?.answer 
+
+  // handles keyboard events
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "ArrowDown") {
+        if (typeof optionClicked !== "number") {
+          setOptionClicked(0)
+        } else {
+          setOptionClicked(prev => {
+            if (prev === questions[activeQuestion].options.length - 1) {
+              return 0
+            } else {
+              return prev! + 1
+            }
+          })
+        }
+      }
+
+      if (event.key === "Enter") {
+        handleKeyBoardOptionClick(questions[activeQuestion].options[optionClicked!], optionClicked!)
+      }
+
+      if (event.key === " " && (typeof optionClicked === "number")) {
+        handleNext()
+      }
+    }
+
+    document.addEventListener("keydown", handleKeyDown)
+
+    return () => document.removeEventListener("keydown", handleKeyDown)
+  }, [optionClicked, activeQuestion])
+  
+  // to select an option on click
+  const handleOptionClick = (item: string, index: number) => {
+    setOptionClicked(index);
+    if (item === questions[activeQuestion].answer) {
+      setIsCorrectAnswer(prev => prev === true ? prev : true);
+    } else {
+      setIsCorrectAnswer(prev => prev === false ? prev : false);
+    }
+  }
+
+  // submits an answer
+  const handleSubmit = () => {
+    setClicked(true)
+    if (typeof optionClicked !== "number") return
+
+    setIsSubmitClicked(true)
+    setIsActualAnswer(actualAnswer)
+    if(isCorrectAnswer) {
+      setResult(prevResult => ({...prevResult, correct: prevResult.correct + 1}))
+    } else {
+      setResult(prevResult => ({...prevResult, wrong: prevResult.wrong + 1}))
+    }
+  }
+
+  // select an option with keyboard
+  const handleKeyBoardOptionClick = (item: string, index: number) => {
+    handleOptionClick(item, index)
+    setKeyBoardClick(true)
+  }
+
+  useEffect(() => {
+    if (keyboardClick) {
+      handleSubmit()
+    }
+    setKeyBoardClick(false)
+  }, [keyboardClick])
+
+
+  // moves to the next question
+  const handleNext = () => {
+    if (activeQuestion === questions.length - 1) {
+      setShowResult(true)
+    } else {
+      setShowResult(false)
+    }
+    setActiveQuestion(prevState => prevState + 1)
+    setClicked(false)
+    setOptionClicked(null)
+    setIsCorrectAnswer(null)
+    setIsSubmitClicked(false)
+    setIsActualAnswer("")
+  }
+
+  const iconName = quizQuestions.icon.split("/").pop()
+  console.log(quizQuestions.icon)
+
+  console.log(iconName)
+  return (
+    <>
+    {!showResult
+    ?(<StyledQuiz>
+      <StyledQuizContent>
+        <StyledNumber isSelected={isSelected}>
+          Question {activeQuestion + 1} of {questions.length}
+        </StyledNumber>
+        <StyledQuestion isSelected={isSelected}>
+          {questions[activeQuestion].question}
+        </StyledQuestion>
+        <StyledProgress>
+          <ProgressBar isSelected={isSelected} progress={activeQuestion} total={questions.length} />
+        </StyledProgress>
+      </StyledQuizContent>
+
+      <StyledOptions>
+        {questions[activeQuestion].options.map((item, index) => (
+          <Card
+           key={index}
+           onClick={() => handleOptionClick(item, index)}
+           selected={index === optionClicked}
+           isCorrectAnswer={isCorrectAnswer}
+           isSubmitClicked={isSubmitClicked}
+           tabIndex={0}
+           isSelected={isSelected}
+           onKeyDown={() => handleKeyBoardOptionClick(item, index)}
+          >
+            <StyledOptionIndex
+              selected={index === optionClicked}
+              isCorrectAnswer={isCorrectAnswer}
+              isSubmitClicked={isSubmitClicked}
+            >
+              {Letters[index]}
+            </StyledOptionIndex>
+            <StyledOption isSelected={isSelected}>
+              {item}
+            </StyledOption>
+            { index === optionClicked && isSubmitClicked && isCorrectAnswer
+            ? <Image src={IconCorrect} alt="correct" width={32} height={32} />
+            : index === optionClicked && isSubmitClicked
+            ? <Image src={IconError} alt="wrong" width={32} height={32} />
+            :  item === isActualAnswer
+            &&  <Image src={IconCorrect} alt="correct" width={32} height={32} />
+            }
+          </Card>
+        ))}
+     {!isSubmitClicked && (typeof optionClicked === "number")
+      ? <Button onClick={handleSubmit}>Submit Answer</Button>
+      : isSubmitClicked && (typeof optionClicked === "number")
+      ? <Button onClick={handleNext}>Next Question</Button>
+      : !isSubmitClicked && (typeof optionClicked !== "number")
+      ? <Button onClick={handleSubmit}>Submit Answer</Button>
+      : isSubmitClicked && (typeof optionClicked !== "number")
+      && <Button onClick={handleSubmit}>Submit Answer</Button>
+      }
+      {clicked && (typeof optionClicked !== "number")
+      && (
+        <ErrorWrapper>
+          <Image src={IconError} alt="wrong" width={32} height={32} />
+          <ErrorMessage>Please select an answer</ErrorMessage>
+        </ErrorWrapper>
+      )}
+      </StyledOptions>
+
+    </StyledQuiz>)
+    : (<StyledQuiz>
+       <StyledTitle>
+        <WelcomeTitle isSelected={isSelected}>Quiz Completed</WelcomeTitle>
+        <StyledSubtitle isSelected={isSelected}>You scored...</StyledSubtitle>
+       </StyledTitle>
+
+      <Result>
+       <ResultCard isSelected={isSelected}>
+        <IconTitleWrapper>
+          {quizSubject === "HTML" && (
+            <HtmlWrapper>
+              <img
+                src={subjectToIcon[quizSubject as keyof typeof subjectToIcon]}
+                alt={quizSubject}
+                width={25}
+                height={25}
+              />
+            </HtmlWrapper>
+          )}
+          {quizSubject === "CSS" && (
+            <CssWrapper>
+              <img
+                src={subjectToIcon[quizSubject as keyof typeof subjectToIcon]}
+                alt={quizSubject}
+                width={25}
+                height={25}
+              />
+            </CssWrapper>
+          )}
+          {quizSubject === "JavaScript" && (
+            <JsWrappper>
+              <img
+                src={subjectToIcon[quizSubject as keyof typeof subjectToIcon]}
+                alt={quizSubject}
+                width={25}
+                height={25}
+              />
+            </JsWrappper>
+          )}
+          {quizSubject === "Accessibility" && (
+            <AcWrapper>
+              <img
+               src={subjectToIcon[quizSubject as keyof typeof subjectToIcon]}
+               alt={quizSubject}
+               width={25}
+               height={25}
+              />
+            </AcWrapper>
+          )}
+          <StyledQuizTitle isSelected={isSelected}>{quizQuestions.title}</StyledQuizTitle>
+        </IconTitleWrapper>
+        <CorrectAnswer isSelected={isSelected}>
+          {result.correct}
+        </CorrectAnswer>
+        <TotalQuestion isSelected={isSelected}>
+          out of {questions.length}
+        </TotalQuestion>
+       </ResultCard>
+       <StyledLink to={`/`}>
+        <ButtonStyle>
+          Play Again
+        </ButtonStyle>
+       </StyledLink>
+      </Result>
+      </StyledQuiz>
+    )}
+    </>
+  )
+}
